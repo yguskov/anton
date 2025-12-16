75876572cad7c879368365f14d5d9af439709f2f
yguskov <yguskov@rssystems.ru>
2025-12-16 20:45:58 +0700

    - profile first version layout

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,6 @@
 import 'package:example/providers/auth_provider.dart';
 import 'package:example/src/app_bar_with_menu.dart';
+import 'package:example/src/widgets/text_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -9,61 +10,126 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-
   @override
   void initState() {
     super.initState();
-      final AuthProvider authProvider = Provider.of<AuthProvider>(context, listen: false);
-    
-      if(authProvider.currentUser == null) {
-          authProvider.fetchCurrentUser();
-      }
-      // final sccess = await authProvider.register();
-      //       dynamic response = await _apiService.login(request);
-      // _currentUser = response.User;
-      // _isLoading = false;
+    final AuthProvider authProvider =
+        Provider.of<AuthProvider>(context, listen: false);
+
+    if (authProvider.currentUser == null) {
+      authProvider.fetchCurrentUser();
+    }
+    // final sccess = await authProvider.register();
+    //       dynamic response = await _apiService.login(request);
+    // _currentUser = response.User;
+    // _isLoading = false;
   }
 
   @override
   Widget build(BuildContext context) {
+    var redButtonStyle = ElevatedButton.styleFrom(
+      backgroundColor: Color(0xFFF76D12),
+      // foregroundColor: Colors.white,
+    );
+    double h20 = 20; // vertical indent
 
-    final AuthProvider authProvider = Provider.of<AuthProvider>(context, listen: true);
+    final AuthProvider authProvider =
+        Provider.of<AuthProvider>(context, listen: true);
 
     return Scaffold(
-            appBar: AntAppBar(
-              title: "Профиль пользователя ${authProvider.currentUser?.email}",
+      appBar: AntAppBar(
+        title: "Профиль пользователя ${authProvider.currentUser?.email}",
+      ),
+      body: Theme(
+        data: Theme.of(context).copyWith(
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 15,
+              ),
+              // backgroundColor: Colors.green,
+              // foregroundColor: Colors.white,
             ),
-            body: LayoutBuilder(
-              builder: (context, constraints) {
-                return Column(
-                  children:  [
-                    Expanded(
-                      child: Center(
-                        child: ConstrainedBox(
-                          constraints: BoxConstraints(maxWidth: 1024),
-                          child: Container(
-                            color: Colors.grey[300], 
-                            child: authProvider.isLoading
-                              ? CircularProgressIndicator(backgroundColor: Colors.white,)
-                              : ListView(children: [
-                              SizedBox(height: 10,),
-                              Text('id: ${authProvider.currentUser?.id}', textAlign: TextAlign.center,),
-                              SizedBox(height: 10,),
-                              Text('Создан: ${authProvider.currentUser?.createdAt}', textAlign: TextAlign.center,), 
-                              ],
-                            ),
-                          ),
-                        ),
+          ),
+        ),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return Column(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(maxWidth: 640),
+                      child: Container(
+                        // color: Colors.grey[300],
+                        child: authProvider.isLoading
+                            ? CircularProgressIndicator(
+                                backgroundColor: Colors.white,
+                              )
+                            : Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 20.0),
+                                child: ListView(
+                                  children: [
+                                    SizedBox(height: 20),
+                                    Center(
+                                      child: ElevatedButton(
+                                        onPressed: edit,
+                                        child: Text('Редактировать данные'),
+                                      ),
+                                    ),
+                                    SizedBox(height: h20),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        ElevatedButton(
+                                            onPressed: edit,
+                                            child: Text('Предпросмотр')),
+                                        SizedBox(width: h20),
+                                        ElevatedButton(
+                                            onPressed: edit,
+                                            child: Text('Получить ссылку'),
+                                            style: redButtonStyle),
+                                      ],
+                                    ),
+                                    SizedBox(height: h20),
+                                    Text(
+                                      'Если затея провалится, то мы можем через наших партнеров подобрать вам, как выдающемуся специалисту, новую работу',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    SizedBox(height: h20),
+                                    TextBar('Оставьте ссылку на ваше резюме'),
+                                    SizedBox(height: h20),
+                                    Text(
+                                      'Создан: ${authProvider.currentUser?.createdAt}',
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ],
+                                ),
+                              ),
                       ),
                     ),
+                  ),
+                ),
 
-                    // Нижняя панель
-                    Container(height: 90, color: Colors.white, child: Center(child: ElevatedButton(onPressed: () {  },
-                    child: Text('Сохранить ')))),
-                  ],
-                );
-              },
-            ),
-          ); 
+                // Нижняя панель
+                Container(
+                    height: 90,
+                    color: Colors.white,
+                    child: Center(
+                        child: ElevatedButton(
+                            onPressed: () {}, child: Text('Сохранить ')))),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  void edit() {
+    Navigator.pushNamed(context, '/');
   }
 }
